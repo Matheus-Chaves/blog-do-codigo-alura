3dfad420d4a14d4ce05d8cb8c2cc211f787c7b67
Matheus Fonseca Chaves <matheusfonsecachaves@gmail.com>
2022-03-05 19:10:13 -0300

    gerando chave JWT com crypto e configurando .env

--- src/usuarios/usuarios-controlador.js
+++ src/usuarios/usuarios-controlador.js
@@ -8,7 +8,7 @@ function criaTokenJWT(usuario) {
   }; //payload que irá compor o token
 
   //assina o token com payload + senha secreta
-  const token = jwt.sign(payload, "senha-secreta");
+  const token = jwt.sign(payload, process.env.CHAVE_JWT);
   return token;
 }
 
