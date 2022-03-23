5e962d927bf42c59c897a31f860062b239246d4c
Matheus Fonseca Chaves <matheusfonsecachaves@gmail.com>
2022-03-23 16:11:40 -0300

    incluindo descrição dos cargos

--- src/middlewares/autorizacao.js
+++ src/middlewares/autorizacao.js
@@ -1,5 +1,10 @@
 //middleware para realizar a autorização de acordo com o cargo do user
 module.exports = (cargosObrigatorios) => (req, res, next) => {
+  /* -- DESCRIÇÃO DOS CARGOS
+    ADMIN -> Acesso a qualquer rota
+    EDITOR -> Acesso somente a rotas dos posts
+    ASSINANTE -> Pode apenas visualizar os posts
+  */
   if (cargosObrigatorios.indexOf(req.user.cargo) === -1) {
     res.status(403).end();
     return;
